28c193558ecc562843bdf85275e9aef7ec9a3b4b
NeoSwitchh <booklord34@gmail.com>
2025-07-29 01:38:45 +0700

    home page setengah jalan

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,52 @@
-import { title } from "@/components/primitives";
+"use client";
+import { Image } from "@heroui/image";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function HomePage() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        <div>
-          <h1 className={title()}>Home</h1>
+    <section className="flex flex-col items-start justify-start gap-4 py-8 md:py-10">
+      <div className="flex flex-row w-full text-start justify-center">
+        <div className="items-center justify-start">
+          <h1 className="">Hi!👋</h1>
+          <p className="">I'm Abiyyu Dzaky Muhammad</p>
+          <span className="w-full">
+          Your backend developer who is passionate about building systems that
+            <Typewriter
+              words={[
+                " is secure"," is optimized", " matter"
+              ]}
+              loop={5}
+              cursor
+
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>
         </div>
+        <div className="justify-end">
+          <Image
+            isBlurred
+            alt="HeroUI Album Cover"
+            className="m-5"
+            src="/meButInACircle.png"
+            width={300}
+          />
+        </div>
+      </div>
+      <div>
+        <h2>About</h2>
+        <p>
+          With hands-on experience developing scalable Laravel-based web
+          applications and secure REST APIs, I've built backend solutions for
+          educational institutions — from localized content management to admin
+          dashboards that empower users. I specialize in SQL-driven data
+          structures, robust user authentication, and clean, maintainable code
+          under version control with Git. <br />
+          Beyond the code, I care deeply about secure system architecture,
+          performance, and continuous improvement. I'm always eager to learn,
+          grow, and tackle new challenges.
+        </p>
       </div>
     </section>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -19,7 +19,7 @@ import { GithubIcon, LinkedinIcon} from "@/components/icons";
 
 export const Navbar = () => {
   return (
-    <HeroUINavbar className="bg-default-100" maxWidth="xl" position="sticky">
+    <HeroUINavbar  maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
