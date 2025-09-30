e43679d666b650722b7a8d512593941e8a35aef1
NeoSwitchh <booklord34@gmail.com>
2025-09-30 22:33:43 +0700

    UPDATE site desc

--- config/site.ts
+++ config/site.ts
@@ -1,39 +1,39 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Abiyyu Dzaky Muhammad - Secundum Scientiam",
-  description: "Make beautiful websites regardless of your design experience.",
-  navItems: [
-    {
-      label: "Home",
-      href: "/",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Contact Me!",
-      href: "/contact",
-    },
-  ],
-// Hamburger menu when view is small
-  navMenuItems: [
-    {
-      label: "Home",
-      href: "/",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Contact Me!",
-      href: "/contact",
-    },
-  ],
-  links: {
-    github: "https://github.com/NeoSwitchh",
-    linkedin: "https://www.linkedin.com/in/abiyyu-dzaky-muhammad-024660209/",
-  },
+	name: "Abiyyu Dzaky Muhammad - Secundum Scientiam",
+	description: "Software Engineer | Full-stack Developer",
+	navItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+		{
+			label: "Projects",
+			href: "/projects",
+		},
+		{
+			label: "Contact Me!",
+			href: "/contact",
+		},
+	],
+	// Hamburger menu when view is small
+	navMenuItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+		{
+			label: "Projects",
+			href: "/projects",
+		},
+		{
+			label: "Contact Me!",
+			href: "/contact",
+		},
+	],
+	links: {
+		github: "https://github.com/NeoSwitchh",
+		linkedin: "https://www.linkedin.com/in/abiyyu-dzaky-muhammad-024660209/",
+	},
 };
